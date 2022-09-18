b467b1d3dd5fe010237b4e2129fa34ddd30771b0
zchan012 <zchan012@e.ntu.edu.sg>
2022-09-18 17:38:49 +0800

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  images: {
+    domains: [
+      "www.google.com",
+      "pixabay.com",
+      "i.picsum.photos",
+      "lh3.googleusercontent.com",
+      "cdn.pixabay.com",
+      "p16-amd-va.tiktokcdn.com",
+      "image.shutterstock.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
